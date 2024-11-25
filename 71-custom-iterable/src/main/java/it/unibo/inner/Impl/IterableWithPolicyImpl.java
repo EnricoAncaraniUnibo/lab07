0108aa47e08eb6e23a328e4ac964ac474c4f9e48
EnricoAncaraniUnibo <enrico.ancarani@studio.unibo.it>
2024-11-25 09:59:13 +0100

    2.0

--- 71-custom-iterable/src/main/java/it/unibo/inner/Impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/Impl/IterableWithPolicyImpl.java
@@ -8,10 +8,22 @@ import it.unibo.inner.api.Predicate;
 public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
     private T[] elems;
     private int index;
+    private Predicate<T> filter;
 
     public IterableWithPolicyImpl(T[] elements) {
+        this(elements, (new Predicate<T>() {
+
+            @Override
+            public boolean test(T elem) {
+                return true;
+            }
+        }));
+    }
+
+    public IterableWithPolicyImpl(T[] elements, Predicate<T> filter) {
         this.elems = elements;
         index = 0;
+        setIterationPolicy(filter);
     }
 
     @Override
@@ -21,8 +33,7 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
 
     @Override
     public void setIterationPolicy(Predicate<T> filter) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setIterationPolicy'");
+        this.filter = filter;
     }
 
     public class InnerIterator implements Iterator<T>{
@@ -30,6 +41,9 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
         @Override
         public boolean hasNext() {
             try {
+                while (!filter.test(elems[index])) {
+                    index++;
+                }
                 if(elems[index] != null) 
                 {
                     return true;
